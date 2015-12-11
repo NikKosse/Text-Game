2e25b2170ac0dd646507b086efe7151da38e5334
NikKosse <nikolas.kosse@gmail.com>
2015-12-11 17:02:12 -0600

    began the player class

---
+++ app/src/main/java/Models/Player.java
@@ -0,0 +1,120 @@
+package Models;
+
+/**
+ * Model for the player.  Contains all stats about the player character
+ */
+public class Player {
+    //player appearance
+    private double weight;
+    private double height;
+    private int gender;
+    private int age;
+    private String race;
+    private String skinColor;
+    private String hairColor;
+    private String eyeColor;
+
+    //player stats
+    private int speed;
+    private int strength;
+    private int intelligence;
+    private int toughness;
+
+//getters and setters for class variables
+    public double getWeight () {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public int getGender() {
+        return gender;
+    }
+
+    public void setGender(int gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getRace() {
+        return race;
+    }
+
+    public void setRace(String race) {
+        this.race = race;
+    }
+
+    public String getSkinColor() {
+        return skinColor;
+    }
+
+    public void setSkinColor(String skinColor) {
+        this.skinColor = skinColor;
+    }
+
+    public String getHairColor() {
+        return hairColor;
+    }
+
+    public void setHairColor(String hairColor) {
+        this.hairColor = hairColor;
+    }
+
+    public String getEyeColor() {
+        return eyeColor;
+    }
+
+    public void setEyeColor(String eyeColor) {
+        this.eyeColor = eyeColor;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public int getIntelligence() {
+        return intelligence;
+    }
+
+    public void setIntelligence(int intelligence) {
+        this.intelligence = intelligence;
+    }
+
+    public int getToughness() {
+        return toughness;
+    }
+
+    public void setToughness(int toughness) {
+        this.toughness = toughness;
+    }
+
+}
--- app/src/main/java/com/kosse/nikolas/textgame/StartActivity.java
+++ app/src/main/java/com/kosse/nikolas/textgame/StartActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -33,5 +34,9 @@ public class StartActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void startNew(View view){
+
     }
 }
