d12c9cb2f5ae987dfefbb859ab98ed2baeb365d3
NikKosse <nikolas.kosse@gmail.com>
2016-02-12 17:04:54 -0600

    won't build when running tests :(

--- app/src/androidTest/java/com/kosse/nikolas/textAdventure/UI_Tests.java
+++ app/src/androidTest/java/com/kosse/nikolas/textAdventure/UI_Tests.java
@@ -1,10 +1,26 @@
 package com.kosse.nikolas.textAdventure;
 
-import org.junit.Test;
+import android.support.test.runner.AndroidJUnit4;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith (AndroidJUnit4.class)
 public class UI_Tests {
     @Test
     public void newGameButtonLaunchesGame(){
-
+        onView(withText("New Game"))
+                .perform(click())
+                .check(doesNotExist());
     }
 }
